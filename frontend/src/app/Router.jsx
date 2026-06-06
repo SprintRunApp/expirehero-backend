ffe1dc4120bd41348cc5b9b9083ee77e1fc29022
SprintRunApp <krzschramm@gmail.com>
2026-06-06 19:57:46 +0200

    opis

--- frontend/src/app/Router.jsx
+++ frontend/src/app/Router.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Login from "../pages/Login";
-import Dashboard from "../pages/Dashboard";
-import Items from "../pages/Items";
-
-import useAuth from "../hooks/useAuth";
-
-import AcceptInvitePage from "../pages/AcceptInvitePage";
-
 export default function Router() {
+    console.log("🔥 ROUTER RENDERED");
+    console.log("PATH:", window.location.pathname);
 
     const { user, loading } = useAuth();
+
+    console.log("AUTH:", { user, loading });
 
     if (loading) return <div>Loading...</div>;
 
     return (
         <BrowserRouter>
             <Routes>
-                
-                <Route path="/invite/:token" element={<AcceptInvitePage />} />
+                <Route
+                    path="/invite/:token"
+                    element={
+                        <div>
+                            <h1>INVITE ROUTE TEST</h1>
+                        </div>
+                    }
+                />
 
                 {!user && (
                     <Route path="*" element={<Login />} />
@@ -30,7 +30,6 @@ export default function Router() {
                         <Route path="/items" element={<Items />} />
                     </>
                 )}
-
             </Routes>
         </BrowserRouter>
     );
